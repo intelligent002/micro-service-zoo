f5d34739dbe58893817d11a561054e6667cfe9b0
Arkady Rosenberg <intelligent002@gmail.com>
2024-09-10 19:09:20 +0300

    db

--- public-back-python-flask/flask_graphql_api/config.py
+++ public-back-python-flask/flask_graphql_api/config.py
@@ -1,10 +1,15 @@
 import os
 
+# Explicitly check if any environment variable is missing
+db_username = os.getenv('DB_USERNAME')
+db_password = os.getenv('DB_PASSWORD')
+db_hostname = os.getenv('DB_HOSTNAME')
+db_database = os.getenv('DB_DATABASE')
+
+if not all([db_username, db_password, db_hostname, db_database]):
+    raise ValueError("One or more environment variables are missing: DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_DATABASE")
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://" +
-                               os.getenv('DB_USERNAME') + ":" +
-                               os.getenv('DB_PASSWORD') + "@" +
-                               os.getenv('DB_HOSTNAME') + ":3306/" +
-                               os.getenv('DB_DATABASE'))
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_username}:{db_password}@{db_hostname}:3306/{db_database}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
