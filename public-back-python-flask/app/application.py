a07f50707a3efdc9ee68ce1aeb97b28de6e93ddc
Arkady Rosenberg <intelligent002@gmail.com>
2024-09-11 23:53:03 +0300

    8002

--- public-back-python-flask/app/application.py
+++ public-back-python-flask/app/application.py
@@ -1,3 +1,6 @@
+import os
+
+import prometheus_client
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 
@@ -5,6 +8,7 @@ from app.config import Config
 from app.config import db
 from app.logger import configure_logging
 from app.routes import routes
+
 
 #
 def create_app():
@@ -19,6 +23,11 @@ def create_app():
 
     # Initialize SQLAlchemy with the Flask app
     db.init_app(app)
+
+    # Check if this is the main Gunicorn process and not a worker
+    if 'gunicorn' in os.environ.get('SERVER_SOFTWARE', ''):
+        if os.getppid() == 1:  # The master process in Gunicorn
+            prometheus_client.start_http_server(8002)
 
     # Initialize Prometheus metrics
     PrometheusMetrics(app)
--- public-back-python-flask/main.py
+++ public-back-python-flask/main.py
@@ -6,8 +6,6 @@ from app.config import Config  # Import the db instance
 
 app = create_app()
 
-print("debug mode is: " + os.getenv('FLASK_DEBUG', 'False').lower() == 'true')
-
 if __name__ == "__main__":
     # Toggle debug mode using an environment variable
     app.run(debug=Config.DEBUG_MODE)
