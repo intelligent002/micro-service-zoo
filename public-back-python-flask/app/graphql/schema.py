6e3ffb12c96a5a5525a2455a2315c0aafd82b4f0
Arkady Rosenberg <intelligent002@gmail.com>
2024-09-11 23:30:13 +0300

    filtering

--- public-back-python-flask/app/graphql/schema.py
+++ public-back-python-flask/app/graphql/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from flask import current_app
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
 from app.graphql.models import Project as ProjectModel, Task as TaskModel
@@ -28,18 +29,54 @@ class Task(SQLAlchemyObjectType):
 
 
 class Query(graphene.ObjectType):
+    # Define the List type for simpler resolvers
     get_projects = graphene.List(Project)
     get_tasks = graphene.List(Task)
 
-    @projects_duration.time()  # Measure the duration of this method
-    def resolve_get_projects(self, info):
-        projects_counter.inc()  # Increment counter
-        return ProjectModel.query.all()
+    # this is currently not exactly working
+    # get_projects = SQLAlchemyConnectionField(Project, name=graphene.String())
+    # get_tasks = SQLAlchemyConnectionField(Task, name=graphene.String())
 
-    @tasks_duration.time()  # Measure the duration of this method
-    def resolve_get_tasks(self, info):
-        tasks_counter.inc()  # Increment counter
-        return TaskModel.query.all()
+    # Measure the duration of this method
+    @projects_duration.time()
+    def resolve_get_projects(self, info, **kwargs):
+        try:
+            # Log the passed arguments
+            current_app.logger.debug(f"Received arguments: {kwargs}")
+            # Increment counter
+            projects_counter.inc()
+            # Get the name filter
+            name = kwargs.get('name', None)
 
+            query = ProjectModel.query
+            if name:
+                current_app.logger.debug(f"Fetching tasks with name filter: {name}")
+                query = query.filter(ProjectModel.name.ilike(f"%{name}%"))
+
+            return query
+        except Exception as e:
+            current_app.logger.error(f"Error while fetching projects {str(e)}")
+            return []
+
+    # Measure the duration of this method
+    @tasks_duration.time()
+    def resolve_get_tasks(self, info, **kwargs):
+        try:
+            # Log the passed arguments
+            current_app.logger.debug(f"Received arguments: {kwargs}")
+            # Increment counter
+            tasks_counter.inc()
+            # Get the name filter
+            name = kwargs.get('name', None)
+
+            query = TaskModel.query
+            if name:
+                current_app.logger.debug(f"Fetching tasks with name filter: {name}")
+                query = query.filter(TaskModel.name.ilike(f"%{name}%"))
+
+            return query
+        except Exception as e:
+            current_app.logger.error(f"Error while fetching tasks {str(e)}")
+            return []
 
 schema = graphene.Schema(query=Query)
