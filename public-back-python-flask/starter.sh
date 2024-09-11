8ef30ec3b9f1b7742af99bb48b0ee4cd1da5288b
Arkady Rosenberg <intelligent002@gmail.com>
2024-09-11 17:00:45 +0300

    fixes

--- public-back-python-flask/starter.sh
+++ public-back-python-flask/starter.sh
@@ -1,1 +1,7 @@
-DB_HOSTNAME = 'mysql-master.intel.r7g.org' DB_USERNAME = 'root' DB_PASSWORD = '007' DB_DATABASE = 'laravel_test' python main.py
+!#/bin/bash
+
+DB_HOSTNAME='mysql-master.intel.r7g.org' \
+DB_USERNAME='root' \
+DB_PASSWORD='007' \
+DB_DATABASE='laravel_test' \
+python main.py
