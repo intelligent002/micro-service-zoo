a80adf1dde10b1823e4aee69f7bf5e174d8fd036
Arkady Rosenberg <intelligent002@gmail.com>
2024-09-19 23:21:33 +0300

    cleanup

--- public-front-typescript-nextjs/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    trailingSlash: true,  // Optional: adds trailing slashes to URLs
-    output: 'export',     // For static export mode
-};
-
-export default nextConfig;
