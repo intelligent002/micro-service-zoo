7783a190ba7f86d1ab594475259de7f0dda639ea
Arkady Rosenberg <intelligent002@gmail.com>
2024-09-29 13:38:00 +0300

    alembic

--- zoo-python-fastapi/app/models/sqlAlchemy/project.py
+++ zoo-python-fastapi/app/models/sqlAlchemy/project.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from . import Base
@@ -9,7 +9,7 @@ from . import Base
 class Project(Base):
     __tablename__ = "projects"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    name: Mapped[str] = mapped_column(index=True, unique=True)
+    name: Mapped[str] = mapped_column(String(255),index=True, unique=True,)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True,
                                                  default=lambda: datetime.now(timezone.utc))
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True,
--- zoo-python-fastapi/app/models/sqlAlchemy/task.py
+++ zoo-python-fastapi/app/models/sqlAlchemy/task.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy import DateTime, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from . import Base
@@ -10,7 +10,7 @@ class Task(Base):
     __tablename__ = "tasks"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     project_id: Mapped[int] = mapped_column(ForeignKey('projects.id'))
-    name: Mapped[str] = mapped_column(index=True, unique=True)
+    name: Mapped[str] = mapped_column(String(255),index=True, unique=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True,
                                                  default=lambda: datetime.now(timezone.utc))
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True,
