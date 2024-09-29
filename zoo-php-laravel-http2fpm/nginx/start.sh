1d04929b6eaa033b71eaa2ca772f1a84f67ce038
Arkady Rosenberg <intelligent002@gmail.com>
2024-09-29 22:00:29 +0300

    other nginxs

--- zoo-php-laravel-http2fpm/nginx/start.sh
+++ zoo-php-laravel-http2fpm/nginx/start.sh
@@ -3,17 +3,38 @@
 # Set a default ports if env vars are not provided
 : "${NGINX_PORT:=8004}"
 : "${ZOO_PHP_LARAVEL_PORT:=8003}"
+: "${SERVICE_NAME:=service}"
+: "${ENVIRONMENT:=undefined}"
 
 
 # Logging configuration of the ports
 echo "Using NGINX_PORT: $NGINX_PORT"
 echo "Using ZOO_PHP_LARAVEL_PORT: $ZOO_PHP_LARAVEL_PORT"
+echo "Using SERVICE_NAME: $SERVICE_NAME"
+echo "Using ENVIRONMENT: $ENVIRONMENT"
 
 # Replace placeholder with the environment variable or default value
-envsubst '$ZOO_PHP_LARAVEL_PORT $NGINX_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '\
+$ZOO_PHP_LARAVEL_PORT \
+$NGINX_PORT \
+$SERVICE_NAME \
+$ENVIRONMENT \
+' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Debugging substitution
+# Replace placeholder with the environment variable or default value
+envsubst '\
+$ZOO_PHP_LARAVEL_PORT \
+$NGINX_PORT \
+$SERVICE_NAME \
+$ENVIRONMENT \
+' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+rm /etc/nginx/conf.d/default.conf.template
+rm /etc/nginx/nginx.conf.template
+
+# Debugging substitutions
 # cat /etc/nginx/conf.d/default.conf
+# cat /etc/nginx/nginx.conf
 
 # Start NGINX
 nginx -g 'daemon off;'
--- zoo-typescript-react/nginx/start.sh
+++ zoo-typescript-react/nginx/start.sh
@@ -2,12 +2,34 @@
 
 # Set a default port if NGINX_PORT is not provided
 : "${NGINX_PORT:=80}"
+: "${SERVICE_NAME:=service}"
+: "${ENVIRONMENT:=undefined}"
 
 # Debugging: Output the port to check if it's set correctly
 echo "Using NGINX_PORT: $NGINX_PORT"
+echo "Using SERVICE_NAME: $SERVICE_NAME"
+echo "Using ENVIRONMENT: $ENVIRONMENT"
 
 # Replace placeholder with the environment variable or default value
-envsubst '$NGINX_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '\
+$NGINX_PORT \
+$SERVICE_NAME \
+$ENVIRONMENT \
+' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Replace placeholder with the environment variable or default value
+envsubst '\
+$NGINX_PORT \
+$SERVICE_NAME \
+$ENVIRONMENT \
+' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+rm /etc/nginx/conf.d/default.conf.template
+rm /etc/nginx/nginx.conf.template
+
+# Debugging substitutions
+# cat /etc/nginx/conf.d/default.conf
+# cat /etc/nginx/nginx.conf
 
 # Start NGINX
 nginx -g 'daemon off;'
