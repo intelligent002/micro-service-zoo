838e15d12b855954160c64f41dbfb80c575544a7
Arkady Rosenberg <intelligent002@gmail.com>
2024-10-05 14:55:58 +0300

    metrics

--- zoo-python-fastapi/app/metrics.py
+++ zoo-python-fastapi/app/metrics.py
@@ -1,4 +1,16 @@
+from prometheus_client import Counter, Histogram
 from prometheus_fastapi_instrumentator import Instrumentator
 
+
 def init_metrics(app):
+    # Initialize Prometheus Instrumentation for FastAPI
     Instrumentator().instrument(app).expose(app)
+
+
+# Define your custom counters using prometheus_client.Counter
+projects_counter = Counter('graphql_get_projects_requests_total', 'Total number of requests to getProjects')
+tasks_counter = Counter('graphql_get_tasks_requests_total', 'Total number of requests to getTasks')
+
+# Define histograms to track request durations using prometheus_client.Histogram
+projects_duration = Histogram('graphql_get_projects_duration_seconds', 'Duration of getProjects method in seconds')
+tasks_duration = Histogram('graphql_get_tasks_duration_seconds', 'Duration of getTasks method in seconds')
