451aa706611393736c0955149b6c2aa7b2d792a6
Arkady Rosenberg <intelligent002@gmail.com>
2024-10-06 12:29:06 +0300

    design

--- zoo-typescript-react/src/components/Task/TaskItem.js
+++ zoo-typescript-react/src/components/Task/TaskItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TaskItem({task}) {
     return (
         <ul>
-            <li key={task.id}>{task.name}</li>
+            <li key={task.id}>{task.created_at}: {task.name}</li>
         </ul>
     );
 }
--- zoo-typescript-react/src/types/taskTypes.ts
+++ zoo-typescript-react/src/types/taskTypes.ts
@@ -2,6 +2,8 @@
 export interface Task {
     id: number;
     name: string;
+    created_at: string;
+    updated_at: string;
 }
 
 // Define the structure of the GraphQL query response
