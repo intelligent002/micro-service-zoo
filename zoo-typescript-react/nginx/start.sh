0d952ec20dd9ed19317706bf088750acb543e4fc
Arkady Rosenberg <intelligent002@gmail.com>
2024-10-07 13:48:55 +0300

    host based routes without prefix

--- zoo-typescript-react/nginx/start.sh
+++ zoo-typescript-react/nginx/start.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# Set a default port if NGINX_PORT is not provided
+# Set a default values if not provided
 : "${NGINX_PORT:=80}"
 : "${SERVICE_NAME:=service}"
 : "${ENVIRONMENT:=undefined}"
+: "${BACKEND_HOSTNAME:=public-api.docker.r7g.org}"
 
-# Debugging: Output the port to check if it's set correctly
+# Debugging: Output to check if it's set correctly
 echo "Using NGINX_PORT: $NGINX_PORT"
 echo "Using SERVICE_NAME: $SERVICE_NAME"
 echo "Using ENVIRONMENT: $ENVIRONMENT"
+echo "Using BACKEND_HOSTNAME: $BACKEND_HOSTNAME"
 
 # Replace placeholder with the environment variable or default value
 envsubst '\
@@ -24,9 +26,16 @@ $SERVICE_NAME \
 $ENVIRONMENT \
 ' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+# Replace placeholder with the environment variable or default value
+jq -r ' .apiUrl =       env.BACKEND_HOSTNAME
+|       .featureFlagX = false
+' /usr/share/nginx/html/config.json > /usr/share/nginx/html/config.json.tmp
+mv -f /usr/share/nginx/html/config.json.tmp /usr/share/nginx/html/config.json
+
 # Debugging substitutions
 # cat /etc/nginx/conf.d/default.conf
 # cat /etc/nginx/nginx.conf
+# cat /usr/share/nginx/html/config.json
 
 # Start NGINX
 nginx -g 'daemon off;'
