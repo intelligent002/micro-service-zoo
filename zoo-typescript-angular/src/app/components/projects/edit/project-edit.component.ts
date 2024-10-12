8a076a15ef0ea0b9674b3bcb2583795ad5ba13e5
Arkady Rosenberg <intelligent002@gmail.com>
2024-10-12 15:03:48 +0300

    focus works

--- zoo-typescript-angular/src/app/components/projects/edit/project-edit.component.ts
+++ zoo-typescript-angular/src/app/components/projects/edit/project-edit.component.ts
@@ -1,11 +1,11 @@
 import {
-  AfterViewInit,
   Component,
   ElementRef,
   EventEmitter,
   Input,
   OnChanges,
   Output,
+  Renderer2,
   SimpleChanges,
   ViewChild
 } from '@angular/core';
@@ -21,44 +21,45 @@ import {ProjectService} from '../../../services/project.service';
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class ProjectEditComponent implements AfterViewInit, OnChanges {
+export class ProjectEditComponent implements OnChanges {
   updateForm!: FormGroup;
 
   @Input() project!: Project;  // Receive the project from the parent
   @ViewChild('projectNameInput') projectNameInput!: ElementRef;
   @Output() projectUpdated = new EventEmitter<Project>();
 
-  constructor(private fb: FormBuilder, private projectService: ProjectService) {
+  constructor(private fb: FormBuilder, private projectService: ProjectService, private renderer: Renderer2) {
   }
 
   // Initialize the form with the project data when the project input changes
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['project'] && changes['project'].currentValue) {  // Corrected access with square brackets
-      const project = changes['project'].currentValue;  // Get the updated project
+    if (changes['project'] && changes['project'].currentValue) {
+      const project = changes['project'].currentValue;
       this.updateForm = this.fb.group({
-        name: [project.name, Validators.required]  // Initialize with project name
+        name: [project.name, Validators.required]
       });
     }
   }
 
+  // Focus the input field after the view is rendered
   ngAfterViewInit(): void {
-    // Automatically focus on the project name input field when the component is loaded
-    if (this.projectNameInput) {
-      this.projectNameInput.nativeElement.focus();
-    }
+    setTimeout(() => {
+      if (this.projectNameInput) {
+        this.renderer.selectRootElement(this.projectNameInput.nativeElement).focus();
+      }
+    });
   }
 
   async updateProject(): Promise<void> {
     if (this.updateForm.valid) {
-      // Emit the updated project
       const updatedProject = {...this.project, ...this.updateForm.value};
       this.projectUpdated.emit(updatedProject);
 
-      // Optionally, call a service to update the project on the server
       this.projectService.updateProject(updatedProject).subscribe({
         next: () => {
           console.log('Project updated successfully');
-        }, error: (error) => {
+        },
+        error: (error) => {
           console.error('Error updating project:', error);
         }
       });
