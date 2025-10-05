35a8dc72b7f1202bd079cd0278b76e47cae74128
Arkady Rosenberg <intelligent002@gmail.com>
2025-10-04 18:33:34 -0700

    health

--- zoo-python-flask/app/routes/health/namespace.py
+++ zoo-python-flask/app/routes/health/namespace.py
@@ -1,4 +1,4 @@
 from flask_restx import Namespace
 
 # Define the namespace for health checks
-ns_health = Namespace('Health', path='/graphql', description='Health related operations')
+ns_health = Namespace('Health', path='/health', description='Health related operations')
