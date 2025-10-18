35fafde8ecc5b7687a52e74e3f15b813b1e68956
Arkady Rosenberg <intelligent002@gmail.com>
2025-10-17 23:48:50 -0700

    fix laravel

--- zoo-php-laravel-http2fpm/nginx/start.sh
+++ zoo-php-laravel-http2fpm/nginx/start.sh
@@ -9,12 +9,14 @@
 
 # Logging configuration of the ports
 echo "Using NGINX_PORT: $NGINX_PORT"
+echo "Using ZOO_PHP_LARAVEL_HOST: $ZOO_PHP_LARAVEL_HOST"
 echo "Using ZOO_PHP_LARAVEL_PORT: $ZOO_PHP_LARAVEL_PORT"
 echo "Using SERVICE_NAME: $SERVICE_NAME"
 echo "Using ENVIRONMENT: $ENVIRONMENT"
 
 # Replace placeholder with the environment variable or default value
 envsubst '\
+$ZOO_PHP_LARAVEL_HOST \
 $ZOO_PHP_LARAVEL_PORT \
 $NGINX_PORT \
 $SERVICE_NAME \
@@ -23,6 +25,7 @@ $ENVIRONMENT \
 
 # Replace placeholder with the environment variable or default value
 envsubst '\
+$ZOO_PHP_LARAVEL_HOST \
 $ZOO_PHP_LARAVEL_PORT \
 $NGINX_PORT \
 $SERVICE_NAME \
